9d8284c68fd8b5a39f5a8ac0f487c69040f84b03
Green Squirrel <greensquirreldragon@protonmail.com>
2022-08-15 04:59:40 -0400

    Added ztext and precosmic module.

---
+++ src/components/Precosmic.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+  <h1>Precosmic</h1>
+
+  <z-text>
+    Precosmic
+  </z-text>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ZText from '@namchee/vue-ztext';
 
 createApp(App).mount('#app')
+app.use(ZText);
