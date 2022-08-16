aec6b519eec16e7014b6118dc0be77a9efb4d56a
Green Squirrel <greensquirreldragon@protonmail.com>
2022-08-15 22:15:56 -0400

    goldenrod

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import Precosmic from './components/Precosmic.vue'
 
 <style>
 body {
-    background-color: lightgray;
+    background-color: darkgoldenrod;
   background-image: 
     radial-gradient(closest-side, rgba(0, 128, 0, 1), rgba(0, 0, 255, 0)),
     radial-gradient(closest-side, rgba(0, 0, 255, 1), rgba(128, 0, 128, 0)),
