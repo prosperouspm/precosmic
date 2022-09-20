142de7520401bc0d84395861c6b0e7c7539f1a12
Green Squirrel <greensquirreldragon@protonmail.com>
2022-09-19 22:06:52 -0400

    Added kimono page.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
 })
+
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        kimono: resolve(__dirname, 'kimono/index.html')
+      }
+    }
+  }
+}
