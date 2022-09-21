41f5d3b251d48842940776f81395486ed17f142c
Green Squirrel <greensquirreldragon@protonmail.com>
2022-09-20 21:10:35 -0400

    Undo last commit.

--- src/router.js
+++ src/router.js
@@ -1,16 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Precosmic from './components/Precosmic.vue'
-import Kimono from "./components/Kimono.vue";
+import Kimono from './components/Kimono.vue';
+import Kimono2 from './pages/Kimono.vue';
 
 
 const About = { template: '<div>About</div>' }
 
 
 export const router = createRouter({
-  history: createWebHistory(),
+    history: createHistory(process.env.VUE_ROUTER_BASE),
   routes: [
     { path: '/', component: Precosmic },
-    { path: "/kimono", component: Kimono },
+    { path: '/kimono', component: Kimono },
+    { path: '/kimono2', component: Kimono2 },
     { path: '/about', component: About },
   ]
 });
