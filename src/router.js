af1af960e52592d1f8248212233e07a4546c7931
Green Squirrel <greensquirreldragon@protonmail.com>
2022-09-20 20:38:08 -0400

    Add home route.

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Precosmic from './components/Precosmic.vue'
 import Kimono from "./views/Kimono.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', component: Precosmic },
     { path: "/kimono", component: Kimono },
   ]
 });
