cc2bbcd50451fa3b57d2a9adb2ef51a49b942925
Green Squirrel <greensquirreldragon@protonmail.com>
2022-09-20 20:41:00 -0400

    Converted Kimono to component.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Precosmic from './components/Precosmic.vue'
-import Kimono from "./views/Kimono.vue";
+import Kimono from "./components/Kimono.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
