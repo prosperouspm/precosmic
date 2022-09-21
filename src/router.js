902cc94f9915949f6dcde25b3f521d45d048db26
Green Squirrel <greensquirreldragon@protonmail.com>
2022-09-20 20:47:54 -0400

    Added about route.

--- src/router.js
+++ src/router.js
@@ -2,10 +2,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import Precosmic from './components/Precosmic.vue'
 import Kimono from "./components/Kimono.vue";
 
+
+const About = { template: '<div>About</div>' }
+
+
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: Precosmic },
     { path: "/kimono", component: Kimono },
+    { path: '/about', component: About },
   ]
 });
