add9b5feb738ebe77550da78b7aafb72e0186924
Green Squirrel <greensquirreldragon@protonmail.com>
2022-09-20 21:07:16 -0400

    Change to createHistory not createWebHistory.

--- src/App.vue
+++ src/App.vue
@@ -12,106 +12,46 @@
   <router-view></router-view>
 </template>
 
+<script>
+  import { useRouter, useRoute } from "vue-router";
+  // import { supabase } from "./supabase";
+  
+  export default {
+    name: "App",
+  
+    setup() {
+      const router = useRouter();
+      const route = useRoute();
+  
+      // router.beforeEach(async (to, from) => {
+      //   // to.path must be checked to prevent infinite redirection
+      //   if (!supabase.auth.user() && to.path.includes("/viewChangeRequest")) {
+      //     // email link clicked
+      //     return { path: "/auth", query: { redirect: to.path } };
+      //   } else if (
+      //     !supabase.auth.user() &&
+      //     to.path !== "/auth" &&
+      //     to.path !== "/register"
+      //   ) {
+      //     return { path: "/auth" };
+      //   } else if (
+      //     supabase.auth.user() &&
+      //     (to.path === "/auth" || to.path === "/register")
+      //   ) {
+      //     return { path: "/" };
+      //   }
+      // });
+    },
+  };
+  </script>
+
 <style>
-body {
-    background-color: white;
-  background-image: 
-    radial-gradient(closest-side, rgba(0, 128, 0, 1), rgba(0, 0, 255, 0)),
-    radial-gradient(closest-side, rgba(0, 0, 255, 1), rgba(128, 0, 128, 0)),
-    radial-gradient(closest-side, rgba(128, 0, 128, 1), rgba(0, 128, 0, 0)),
-    radial-gradient(closest-side, rgba(0, 128, 0, 1), rgba(0, 0, 255, 0)),
-    radial-gradient(closest-side, rgba(0, 0, 255, 1), rgba(128, 0, 128, 0));
-  background-size: 
-    130vmax 130vmax,
-    80vmax 80vmax,
-    90vmax 90vmax,
-    110vmax 110vmax,
-    90vmax 90vmax;
-  background-position:
-    -80vmax -80vmax,
-    60vmax -30vmax,
-    10vmax 10vmax,
-    -30vmax -10vmax,
-    50vmax 50vmax;
-  background-repeat: no-repeat;
-  animation: 32s movement linear infinite, 157s colorchange infinite;
-}
-
-body::after {
-  content: '';
-  display: block;
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-}
-
-@keyframes movement {
-  0%, 100% {
-    background-size: 
-      130vmax 130vmax,
-      80vmax 80vmax,
-      90vmax 90vmax,
-      110vmax 110vmax,
-      90vmax 90vmax;
-    background-position:
-      -80vmax -80vmax,
-      60vmax -30vmax,
-      10vmax 10vmax,
-      -30vmax -10vmax,
-      50vmax 50vmax;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
   }
-  25% {
-    background-size: 
-      100vmax 100vmax,
-      90vmax 90vmax,
-      100vmax 100vmax,
-      90vmax 90vmax,
-      60vmax 60vmax;
-    background-position:
-      -60vmax -90vmax,
-      50vmax -40vmax,
-      0vmax -20vmax,
-      -40vmax -20vmax,
-      40vmax 60vmax;
-  }
-  50% {
-    background-size: 
-      80vmax 80vmax,
-      110vmax 110vmax,
-      80vmax 80vmax,
-      60vmax 60vmax,
-      80vmax 80vmax;
-    background-position:
-      -50vmax -70vmax,
-      40vmax -30vmax,
-      10vmax 0vmax,
-      20vmax 10vmax,
-      30vmax 70vmax;
-  }
-  75% {
-    background-size: 
-      90vmax 90vmax,
-      90vmax 90vmax,
-      100vmax 100vmax,
-      90vmax 90vmax,
-      70vmax 70vmax;
-    background-position:
-      -50vmax -40vmax,
-      50vmax -30vmax,
-      20vmax 0vmax,
-      -10vmax 10vmax,
-      40vmax 60vmax;
-  }
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
